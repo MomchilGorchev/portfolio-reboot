e19344ad2c9511b740f03ce2a540150091c68a3d
Momchil Gorchev <momchil.gorchev@gmail.com>
2020-04-14 08:27:42 +0100

    Updates

---
+++ precache-manifest.a6994a2201fe3deaab5447f30089df53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51dd514db4e0b4ae9863cc7a0c66eb8",
+    "url": "/portfolio-reboot/index.html"
+  },
+  {
+    "revision": "e22863fddd572604af42",
+    "url": "/portfolio-reboot/static/css/main.b3101f16.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/portfolio-reboot/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "e22863fddd572604af42",
+    "url": "/portfolio-reboot/static/js/main.b3d06717.chunk.js"
+  },
+  {
+    "revision": "0bcb981538c86b759744",
+    "url": "/portfolio-reboot/static/js/runtime~main.7c005b3b.js"
+  }
+]);
